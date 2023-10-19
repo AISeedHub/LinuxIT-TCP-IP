06100b6703373395af17e54e55159d04ee864b36
Andrew <andrewlee1807@gmail.com>
2023-10-19 20:13:58 +0900

    tcpip with selectors

--- README.md
+++ README.md
@@ -1,14 +1,72 @@
 # Socket with asyncio
 
 Lý do sử dụng asyncio with non-blocking socket `socket.setblocking(flag=False)` instead of threading:
+
 - Threading: mỗi kết nối sẽ tạo ra 1 thread mới, nếu có nhiều kết nối thì sẽ tạo ra nhiều thread, dẫn đến tốn tài nguyên
-- Asyncio: sử dụng 1 thread duy nhất, nó sẽ chạy từng task một, khi task nào đang chờ thì nó sẽ chuyển sang task khác, khi task đó hoàn thành thì nó sẽ quay lại task đang chờ trước đó
-- Asyncio sử dụng non-blocking socket, nghĩa là khi có nhiều kết nối đến, nó sẽ chuyển đổi giữa các kết nối đó, không cần phải chờ kết nối trước đó hoàn thành
-- Và trên server các tác vụ chỉ là điều kiển một đối tượng duy nhất là Deep Learning Model, và các kết nối đến từ Clients để giám sát nó (nghĩa là không phát sinh thêm các hành động độc lập khác ở mỗi kết nối mới được thiết lập) nên không cần phải sử dụng nhiều thread. 
+- Asyncio: sử dụng 1 thread duy nhất, nó sẽ chạy từng task một, khi task nào đang chờ thì nó sẽ chuyển sang task khác,
+  khi task đó hoàn thành thì nó sẽ quay lại task đang chờ trước đó
+- Asyncio sử dụng non-blocking socket, nghĩa là khi có nhiều kết nối đến, nó sẽ chuyển đổi giữa các kết nối đó, không
+  cần phải chờ kết nối trước đó hoàn thành
+- Và trên server các tác vụ chỉ là điều kiển một đối tượng duy nhất là Deep Learning Model, và các kết nối đến từ
+  Clients để giám sát nó (nghĩa là không phát sinh thêm các hành động độc lập khác ở mỗi kết nối mới được thiết lập) nên
+  không cần phải sử dụng nhiều thread.
+
+# Socket with `selectors` lib in python
+[Python module selectors](https://mathspp.com/blog/til/022)
+
+```python
+# server.py
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(("localhost", 7342))
+server.listen()
+print("About to accept:")
+server.accept()
+print("Call to `accept` returned.")
+```
+
+If you run this with python server.py, the message "About to accept:" gets printed, but not the message "Call
+to `accept` returned.". Why? Because there is no connection to be accept, and so the call to accept blocks, waiting for
+a connection.
+Thus, we can't just call the method recv on the different clients we have, because if one doesn't have anything for us,
+we will block! A common way to deal with this is by spawning a thread for each client, and do the blocking calls in
+those separate threads.
+
+Another way to deal with this is with the module `selectors`.
+```python
+
+# server.py
+
+import selectors
+import socket
+
+# Set up the server
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(("localhost", 7342))
+server.listen()
+
+# Set up the selectors "bag of sockets"
+selector = selectors.DefaultSelector()
+selector.register(server, selectors.EVENT_READ)
+
+while True:
+    events = selector.select()
+    for key, _ in events:
+        sock = key.fileobj
+        print("About to accept.")
+        client, _ = sock.accept()
+        print("Accepted.")
+```
 
 ## Response Error
-- `CodeError`: 41(0x29) - `Error`: 0x29 - `Message`: `Invalid request code`(Command is not understanding) or Many request with same code
+
+- `CodeError`: 41(0x29) - `Error`: 0x29 - `Message`: `Invalid request code`(Command is not understanding) or Many
+  request with same code
 - `CodeError`: 42(0x2A) - `Error`: 0x2A - `Message`: `Invalid JSON format``
+
 ---
+
 ### Citations
+
 [GITHUB](https://github.com/DevStarSJ/Study/blob/master/Blog/Python/Socket/02.chat.md)
--- tcpip.py
+++ tcpip.py
@@ -1,85 +1,160 @@
-from socket import socket, SO_REUSEADDR, SOL_SOCKET
-from asyncio import Task, coroutine
-import asyncio
+# from socket import socket, SO_REUSEADDR, SOL_SOCKET
+# from asyncio import Task, coroutine
+import selectors
+import socket
 
 
 class Peer(object):
     def __init__(self, server, sock, name):
-        self.loop = server.loop
         self._taskManager = server._taskManager
         self.name = name
         self._sock = sock
         self._server = server
-        self.task = Task(self._peer_handler())
+        self.selector = server.selector
 
-    def send(self, data):
-        # return self.loop.sock_sendall(self._sock, data.encode(ENCODING))
-        return self.loop.sock_sendall(self._sock, data.encode(self._server._config["ENCODING"]))
+    def read_data(self, connection, mask):
+        client_address = connection.getpeername()
+        print('read({})'.format(client_address))
+        data = connection.recv(self._server._config["BUFFER_SIZE"])
+        if data:
+            print('  received {!r}'.format(data))
+            if data.strip().lower() == b'close':
+                print('  closing')
+                self.selector.unregister(connection)
+                connection.close()
+                self._server._peers.remove(self)
+            else:
+                connection.sendall(data)
+        else:
+            print('closing')
+            self.selector.unregister(connection)
+            connection.close()
 
-    @coroutine
-    def _peer_handler(self):
+    def broadcast(self, data):
+        return self._sock.sendall(data.encode(self._server._config["ENCODING"]))
+        # return self.loop.sock_sendall(self._sock, data.encode(self._server._config["ENCODING"]))
+
+    # @coroutine
+    def peer_handler(self, connection, mask):
         try:
-            # await self._peer_loop()
-            yield from self._peer_loop()
-        except IOError:
-            pass
-        finally:
+            print("Peer handler")
+            self._peer_loop()
+        except IOError as e:
+            print("IOError: ", e)
+            # finally:
             self._server.remove(self)
 
     # @coroutine
-    async def _peer_loop(self):
-        while True:
-            buf = await self.loop.sock_recv(self._sock, self._server._config["BUFFER_SIZE"])
-            if buf == b'':
-                break
+    def _peer_loop(self, ):
+        # while True:
+        # buf = yield from self.loop.sock_recv(self._sock, self._server._config["BUFFER_SIZE"])
+        data = self._sock.recv(self._server._config["BUFFER_SIZE"])
+        # buf = await self.loop.sock_recv(self._sock, self._server._config["BUFFER_SIZE"])
+        if data:
+            data = data.decode(self._server._config["ENCODING"])
+            message = '<----------- %s: %s' % (self.name, data)
 
-            data = buf.decode(self._server._config["ENCODING"])
-            message = 'Receive data from %s: %s' % (self.name, data)
+            if data.lower() == 'close':
+                print('  closing')
+                self.selector.unregister(self._sock)
+                self._sock.close()
+                self._server._peers.remove(self)
+            else:
+                self.broadcast(data)
+
             print(message)
-
             json_response = self._taskManager.distribute_task(data)
             print("json_response: ", json_response)
             # Return the message to the client
-            await self._server.broadcast(json_response)
-
+            self._server.broadcast(json_response)
+        else:
+            print("Connection closed by client")
+            # self._server.remove(self)
+            # break
 
 
 class Server(object):
     def __init__(self, manager):
+        self.selector = selectors.DefaultSelector()
         self._taskManager = manager
         self.loop = manager.loop  # Event loops schedules and manages tasks
         self._config = manager.config
-        self._serv_sock = socket()
-        self._serv_sock.setblocking(0)
-        self._serv_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-        self._serv_sock.bind(('', self._config["PORT"]))
+        # Set up the server
+        self._serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._serv_sock.setblocking(False)
+        # self._serv_sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        self._serv_sock.bind((self._config["IP"], self._config["PORT"]))
         self._serv_sock.listen(self._config["MAX_CONNECTIONS"])
+        # Set up the selectors "bag of sockets"
         self._peers = []
+
+    def accept_connection(self, sock, mask):
+        new_connection, addr = sock.accept()
+        print('WELLCOME bro: ({})'.format(addr))
+        peer = Peer(self, new_connection, addr)
+        new_connection.setblocking(False)
+        self._peers.append(peer)
+        self.selector.register(new_connection, selectors.EVENT_READ, peer.peer_handler)
+        # self.selector.register(new_connection, selectors.EVENT_READ, self.read_data)
+
+    # def read_data(self, connection, mask):
+    #     client_address = connection.getpeername()
+    #     print("List all connection: ", len(self._peers))
+    #     for i in range(len(self._peers)):
+    #         print(self._peers[i])
+    #     print('read({})'.format(client_address))
+    #     data = connection.recv(1024)
+    #     if data:
+    #         print('  received {!r}'.format(data))
+    #         if data.strip().lower() == b'close':
+    #             print('  closing')
+    #             self.selector.unregister(connection)
+    #             connection.close()
+    #             self._peers.remove(connection)
+    #         else:
+    #             connection.sendall(data)
+    #     else:
+    #         print('closing')
+    #         self.selector.unregister(connection)
+    #         connection.close()
 
     def start(self):
         print("Server Address:", self._config["IP"] + ":" + str(self._config["PORT"]))
         print("Start the server...")
-        return Task(self._server(), name="server")
+        self.selector.register(self._serv_sock, selectors.EVENT_READ, self.accept_connection)
+        try:
+            while True:
+                print("Waiting for connection...")
+                for key, mask in self.selector.select():
+                    callback = key.data
+                    callback(key.fileobj, mask)
+        except KeyboardInterrupt:
+            print('shutting down')
+            self.selector.close()
+
+        # return Task(self._server(), name="server")
 
     def start_input(self):
         print("Start input listener...")
-        return Task(self.input_peers_info(), name="input_peers_info")
+        # return Task(self.input_peers_info(), name="input_peers_info")
 
     def remove(self, peer):
-        self._peers.remove(peer)
+        print("Removing peer... ", peer.name)
         print("Bye bye %s" % peer.name)
         self.broadcast('Peer %s quit!\n' % peer.name)
+        self.peer._sock.close()
+        print(f"Connection with {self.name} closed.")
+        self._peers.remove(peer)
 
     def broadcast(self, message):
         print("Broadcasting message...")
         for peer in self._peers:
-            print(f'Send message "{message}" to {peer.name}')
+            print(f'----------> {peer.name} : "{message}"')
             peer._sock.sendall(message.encode(self._config["ENCODING"]))
             # peer.send(message)
             print("Done sending")
 
-
-    @coroutine
+    # @coroutine
     def _server(self):
         while True:
             # peer_sock, peer_name = await self.loop.sock_accept(self._serv_sock)
@@ -89,22 +164,24 @@ class Server(object):
             peer = Peer(self, peer_sock, peer_name)
             self._peers.append(peer)
 
-            message = 'Peer %s connected!\n' % (peer.name,)
-            print(message)
+            # message = 'WELLCOME Peer %s connected!\n' % (peer.name,)
+            # print(message)
 
-            self.broadcast(message)
+            # self.broadcast(message)
+            self.remove(peer)
+            print("Pass remove")
 
-    async def input_peers_info(self):
-        # Listen for user input and process it
-        while True:
-            print("Enter 'check' to see connected peers: ")
-            user_input = await self.loop.run_in_executor(None, input)
-            if user_input == "check":
-                print("---------------------------------")
-                print("Peers in the network: ")
-                print("Number of Peers: ", len(self._peers))
-                for i in range(len(self._peers)):
-                    print(self._peers[i].name)
-                print("---------------------------------")
-            elif user_input == "task":
-                self._taskManager.check_task()
+    # async def input_peers_info(self):
+    #     # Listen for user input and process it
+    #     while True:
+    #         print("Enter 'check' to see connected peers: ")
+    #         user_input = await self.loop.run_in_executor(None, input)
+    #         if user_input == "check":
+    #             print("---------------------------------")
+    #             print("Peers in the network: ")
+    #             print("Number of Peers: ", len(self._peers))
+    #             for i in range(len(self._peers)):
+    #                 print(self._peers[i].name)
+    #             print("---------------------------------")
+    #         elif user_input == "task":
+    #             self._taskManager.check_task()
