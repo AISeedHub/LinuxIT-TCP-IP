5a425567bb3622508489b124908322bf0e018c19
Ka0Ri <dtvu1707@gmail.com>
2023-12-01 21:24:07 +0900

    add yolov5, model, config and requirements

--- model.py
+++ model.py
@@ -2,6 +2,9 @@ import numpy as np
 import onnxruntime as ort
 from utils import segmentaion_to_crop, load_image, convert_image
 
+import cv2
+import torch
+import yaml
 
 class DLModel:
     def __init__(self, dir_model_preprocessing, root_dir) -> None:
@@ -45,6 +48,41 @@ class DLModel:
         self.model_name = None
         self.model_classification = None
 
-    
+class PearDetectionModel():
 
+    def __init__(self, config) -> None:
+        self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        self.model = torch.hub.load('ultralytics/yolov5', 'custom', path=config['model_path'])
+        self.model.to(self.device)
+
+        self.names = config['classes']
+
+    def detect(self, img):
+        img = self._preporcess(img)
+        results = self.model(img)
+        return results.pred[0].numpy()
+    
+    def inference(self, img):
+        pred = self.detect(img)
+        labels = [self.names[int(cat)] for _,_,_,_,_,cat in pred]
+        if 'normal_pear_bbox' in labels:
+            return 1
+        else:
+            return 0
+    
+    def _preporcess(self, img):
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        return img
+
+    
+if __name__ == '__main__':
+
+    with open('yolo_config.yml', 'r') as f:
+        config = yaml.safe_load(f)
+
+    model = PearDetectionModel(config)
+    for image in []:
+        img = cv2.imread(image)
+       
+        print(model.inference(img))
 
